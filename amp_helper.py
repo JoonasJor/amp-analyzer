9503019ac1a33268ada19db7e03efa4bbe1939d4
bobtheburgerflipper <juunas.99@gmail.com>
2024-03-07 17:34:11 +0200

    resultti toimii

--- amp_helper.py
+++ amp_helper.py
@@ -28,9 +28,11 @@ class MyMainWindow(QMainWindow):
         self.canvas = PlotCanvas(self.plotWidget)
         layout = QVBoxLayout(self.plotWidget)
         layout.addWidget(self.canvas)
+        layout.setContentsMargins(2, 2, 2, 2)  # Set margins to 0
 
         navigation_toolbar = NavigationToolbar(self.canvas, self)
-        self.addToolBar(Qt.ToolBarArea.BottomToolBarArea, navigation_toolbar)
+        #navigation_toolbar.setStyleSheet("QToolBar { border: 0px; }")
+        self.verticalLayout_toolbox.addWidget(navigation_toolbar)
 
         self.on_button_add_dataset_clicked()
 
@@ -70,8 +72,8 @@ class MyMainWindow(QMainWindow):
             return
 
         try:
-            times = data['time/s']
-            currents = data['current/µA']
+            times = data['time/s'].tolist()
+            currents = data['current/µA'].tolist()
         except Exception as e: 
             QMessageBox.warning(self, "Error", "Data is in wrong format.")
             print(e)
@@ -79,7 +81,7 @@ class MyMainWindow(QMainWindow):
         
         #print(len(times))
         #print(len(currents))
-        self.canvas.add_dataset(name, times, currents)
+        self.canvas.add_dataset(name, times, currents, concentration, notes)
 
 
     def parse_clipboard_data(self):
@@ -95,20 +97,24 @@ class MyMainWindow(QMainWindow):
         return data_frame
 
 class PlotCanvas(FigureCanvas):
-    def __init__(self, parent=None, width=5, height=4, dpi=100): 
-        self.figure, self.axes = plt.subplots()
+    def __init__(self, parent=None): 
+        self.figure, (self.axes1, self.axes2) = plt.subplots(1, 2)
         super().__init__(self.figure)
         self.setParent(parent)
+        #self.figure.subplots_adjust(left=0.1, right=0.98, bottom=0.08, top=0.98)
+        self.figure.tight_layout(pad=0.4)
+        #self.figure.subplots_adjust(hspace=0.5)
+
         self.datasets = {}  # Dictionary to store datasets
         self.span = None
         self.span_initialized = False
 
-
     def onselect(self, vmin, vmax):
         print("span:", self.span.extents)
+        if len(self.datasets) > 1: self.plot_results() # EI PÄIVITÄ
 
     def create_span_selector(self, snaps):
-        self.span = SpanSelector(self.axes, 
+        self.span = SpanSelector(self.axes1, 
                                  self.onselect,
                                  'horizontal', 
                                  useblit=True, # For faster canvas updates
@@ -119,44 +125,85 @@ class PlotCanvas(FigureCanvas):
                                  grab_range=6,
                                  snap_values=snaps) # Snap to time values  
         
-    def add_dataset(self, label, x, y):
+    def add_dataset(self, label, times, currents, concentration, notes):
         # Add a new dataset or update existing one
-        self.datasets[label] = (x, y)
+        self.datasets[label] = (times, currents, concentration, notes)
+        print(self.datasets[label][2])
 
-        # Clear existing plot
-        self.axes.clear()
-
-        # Plot each dataset
-        for label, data in self.datasets.items():
-            x, y = data
-            self.axes.plot(x, y, label=label)
-            #import numpy as np; import pandas as pd; import pyperclip; pyperclip.copy(pd.DataFrame(np.array(y).reshape(-1,1)).to_csv(index=False, header=False))
-            #print(y)
-        
-        # Set title and legend and turn on grid
-        self.axes.set_title('Matplotlib Plot')
-        self.axes.legend()
-        self.axes.grid(True)
-
-        # Set labels for axes
-        self.axes.set_ylabel("current/µA")
-        self.axes.set_xlabel("time/s")
-        
-        # Set number of ticks on the x and y axes
-        self.axes.xaxis.set_major_locator(plt.MaxNLocator(10))
-        self.axes.yaxis.set_major_locator(plt.MaxNLocator(10))
-
+        self.plot_data()
+        if len(self.datasets) > 1: 
+            self.plot_results()
         self.draw()
 
         # Set initial span coords
         if not self.span_initialized: 
-            self.create_span_selector(x) # Time values from first dataset
-            last_value = max(x)
+            self.create_span_selector(np.array(times)) # Time values from first dataset
+            last_value = times[-1]
             span_right = last_value
             span_left = round(float(last_value) * 0.9, ndigits=1)
             self.span.extents = (span_left, span_right)
             self.span_initialized = True
-            print(self.span.extents)
+            #print(self.span.extents)
+        
+    def plot_results(self):
+        # Initialize lists to store the average current and concentration
+        avg_currents = []
+        concentrations = []
+
+        # Iterate over each dataset
+        for times, currents, concentration, _ in self.datasets.values():
+            # Convert lists to numpy arrays
+            times = np.array(times)
+            currents = np.array(currents)
+
+            # Find indices where times fall within the specified range
+            indices = np.where((times >= self.span.extents[0]) & (times <= self.span.extents[1]))[0]
+
+            # Calculate the average current for the specified range
+            avg_current = np.mean(currents[indices])
+
+            # Append the average current and concentration to the lists
+            avg_currents.append(avg_current)
+            concentrations.append(concentration)
+
+        # Convert lists to numpy arrays
+        avg_currents = np.array(avg_currents)
+        concentrations = np.array(concentrations, dtype=float)
+        x_ticks = np.arange(start=min(concentrations), stop=max(concentrations) + 1)
+        #print(x_values)
+
+        self.axes2.clear()
+        self.axes2.plot(concentrations, avg_currents, marker='o')
+        self.axes2.grid(True)
+        self.axes2.set_ylabel("current(µA)")
+        self.axes2.set_xlabel("concentration(mM)")
+        self.axes2.xaxis.set_major_locator(plt.FixedLocator(x_ticks))
+        self.axes2.yaxis.set_major_locator(plt.MaxNLocator(10))
+
+    def plot_data(self):
+        # Clear existing plot
+        self.axes1.clear()
+
+        # Plot each dataset
+        for label, data in self.datasets.items():
+            times, currents, concentration, notes = data
+            self.axes1.plot(times, currents, label=label)
+            #import numpy as np; import pandas as pd; import pyperclip; pyperclip.copy(pd.DataFrame(np.array(y).reshape(-1,1)).to_csv(index=False, header=False))
+            #print(y)
+        
+        # Set title and legend and turn on grid
+        #self.axes.set_title('Matplotlib Plot')
+        self.axes1.legend()
+        self.axes1.grid(True)
+
+        # Set labels for axes
+        self.axes1.set_ylabel("current(µA)")
+        self.axes1.set_xlabel("time(s)")
+        
+        # Set number of ticks on the x and y axes
+        self.axes1.xaxis.set_major_locator(plt.MaxNLocator(10))
+        self.axes1.yaxis.set_major_locator(plt.MaxNLocator(10))
+
 
 def main():
     app = QApplication(sys.argv)
